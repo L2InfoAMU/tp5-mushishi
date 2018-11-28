cf8a365212c47aba7c20e7c80b4736dd5f06d181
asibiak <aymeric.sibiak@etu.univ-amu.fr>
2018-11-28 19:40:04 +0100

    Tache 2 3.1

---
+++ src/main/java/image/PaletteRasterImage.java
@@ -0,0 +1,87 @@
+package image;
+
+import javafx.scene.paint.Color;
+import util.Matrices;
+
+public class PaletteRasterImage implements Image{
+    public Color[][] pixels;
+    public int width;
+    public int height;
+
+    public PaletteRasterImage(Color color, int width, int height){
+        this.height = height;
+        this.width = width;
+
+        createRepresentation();
+
+        setPixelsColor(color);
+    }
+
+    public PaletteRasterImage(Color[][] pixels){
+        Matrices.requiresNonNull(pixels);
+        Matrices.requiresNonZeroDimensions(pixels);
+
+        this.height = Matrices.getColumnCount(pixels); // Méthode malhonette , nomage anbigue
+        this.width = Matrices.getRowCount(pixels); // Méthode malhonette , nomage anbigue
+
+        this.pixels = pixels;
+    }
+
+    public void createRepresentation(){
+        this.pixels = new Color[this.width][this.height];
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+        pixels[x][y]= color;
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return pixels[x][y];
+    }
+
+    private void setPixelsColor(Color[][] pixels){
+        int x = Matrices.getRowCount(pixels);
+        int y = Matrices.getColumnCount(pixels);
+
+        for (int i = 0 ; i < x ; i++){
+            for (int j = 0; j < y ; j++){
+                setPixelColor(pixels[x][y],x,y);
+            }
+        }
+    }
+
+    private void setPixelsColor(Color color){
+        for (int x = 0 ; x < width ; x++){
+            for(int y = 0; y < height ; y++){
+                setPixelColor(color,x,y);
+            }
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    protected void setWidth(int width){
+        if ( width < 0 ) throw new ArrayIndexOutOfBoundsException();
+
+        this.width = width;
+        createRepresentation();
+
+    }
+
+    protected void setHeight(int height){
+        if ( height < 0 ) throw new ArrayIndexOutOfBoundsException();
+
+        this.height = height;
+        createRepresentation();
+    }
+
+}
